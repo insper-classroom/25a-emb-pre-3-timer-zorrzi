86a75cc15fc506a9d7432bcc219fbdeecf7dc8b1
eduardo <eduardovz@al.insper.edu.br>
2025-03-09 21:42:29 -0300

    feat: exe1

--- exe1/main.c
+++ exe1/main.c
@@ -6,6 +6,9 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile int g_timer_0 = 0;
+volatile bool timer_running = false;
+
 
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) { // fall edge
@@ -14,8 +17,17 @@ void btn_callback(uint gpio, uint32_t events) {
     }
 }
 
+bool timer_0_callback(repeating_timer_t *rt) {
+    g_timer_0 = 1;
+    return true;    
+}
+
+
 int main() {
     stdio_init_all();
+
+    repeating_timer_t timer_0;
+
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
@@ -30,6 +42,27 @@ int main() {
 
         if (flag_f_r) {
             flag_f_r = 0;
+
+
+            if (!timer_running){
+                if (!add_repeating_timer_ms(500, timer_0_callback, NULL, &timer_0)) {
+                    printf("Failed to add timer\n");
+                } else {
+                    timer_running = true;
+                }
+            }
+
+            else{
+                cancel_repeating_timer(&timer_0);
+                gpio_put(LED_PIN_R, 0);
+                timer_running = false;
+            }    
         }
+
+        if(g_timer_0){
+            g_timer_0 = 0;
+            gpio_xor_mask(1u << LED_PIN_R);        }
     }
+
+
 }
